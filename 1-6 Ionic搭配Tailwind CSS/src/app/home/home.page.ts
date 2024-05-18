307f0e13cdf7cbd2420ab47100d250a4b44a85dd
MomoChenIsMe <momochenisme@gmail.com>
2024-05-18 11:25:26 +0800

    add 1-6 Ionic搭配Tailwind CSS source code

---
+++ 1-6 Ionic搭配Tailwind CSS/src/app/home/home.page.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonItem,
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonItem],
+})
+export class HomePage {
+  constructor() {}
+}
