a20a717c423762ca5116f92275f65c7c89a73047
MomoChenIsMe <momochenisme@gmail.com>
2024-06-05 22:47:17 +0800

    3-7移除測試程式碼

--- 3-7 為API加入讀取狀態和動畫/src/app/home/home.page.ts
+++ 3-7 為API加入讀取狀態和動畫/src/app/home/home.page.ts
@@ -1,6 +1,4 @@
-import { StatusService } from './../services/status.service';
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   IonHeader,
   IonToolbar,
@@ -11,7 +9,6 @@ import {
 } from '@ionic/angular/standalone';
 import { Robot3dComponent } from '../components/robot3d/robot3d.component';
 import { VoicerecordingComponent } from '../components/voicerecording/voicerecording.component';
-import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -29,26 +26,6 @@ import { switchMap } from 'rxjs';
     VoicerecordingComponent,
   ],
 })
-export class HomePage implements OnInit {
-  constructor(
-    private _statusService: StatusService,
-    private httpClient: HttpClient
-  ) {}
-
-  ngOnInit() {
-    this.httpClient
-      .get('https://localhost:7086/AIEnglishTurtor')
-      .pipe(
-        switchMap(() =>
-          this.httpClient.get('https://localhost:7086/AIEnglishTurtor')
-        ),
-        switchMap(() =>
-          this.httpClient.get('https://localhost:7086/AIEnglishTurtor')
-        ),
-        switchMap(() =>
-          this.httpClient.get('https://localhost:7086/AIEnglishTurtor')
-        )
-      )
-      .subscribe();
-  }
+export class HomePage {
+  constructor() {}
 }
