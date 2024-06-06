5e6b7248c450f26ad3021426f5aa6fc2b1a4d26e
MomoChenIsMe <momochenisme@gmail.com>
2024-06-06 23:06:16 +0800

    Add HttpInterceptor JSDoc

--- 3-8 HttpInterceptor的進階使用技巧/src/app/httpInterceptors/http.interceptor.functions.ts
+++ 3-8 HttpInterceptor的進階使用技巧/src/app/httpInterceptors/http.interceptor.functions.ts
@@ -9,6 +9,13 @@ import { Observable, finalize } from 'rxjs';
 import { StatusService } from '../services/status.service';
 import { environment } from 'src/environments/environment';
 
+/**
+ * HttpInterceptor，用於在請求開始和結束時更新讀取狀態
+ *
+ * @param {HttpRequest<unknown>} req - 要攔截的 HTTP 請求。
+ * @param {HttpHandlerFn} next - 下一個攔截器函數。
+ * @returns {Observable<HttpEvent<unknown>>} - 包含 HTTP 事件的 Observable 對象。
+ */
 export const loadingHttpInterceptor: HttpInterceptorFn = (
   req: HttpRequest<unknown>,
   next: HttpHandlerFn
@@ -22,6 +29,13 @@ export const loadingHttpInterceptor: HttpInterceptorFn = (
   );
 };
 
+/**
+ * HttpInterceptor，用於為每個請求加入 Bearer token。
+ *
+ * @param {HttpRequest<unknown>} req - 要攔截的 HTTP 請求。
+ * @param {HttpHandlerFn} next - 下一個攔截器函數。
+ * @returns {Observable<HttpEvent<unknown>>} - 包含 HTTP 事件的 Observable 對象。
+ */
 export const bearerTokenHttpInterceptor: HttpInterceptorFn = (
   req: HttpRequest<unknown>,
   next: HttpHandlerFn
@@ -36,6 +50,13 @@ export const bearerTokenHttpInterceptor: HttpInterceptorFn = (
   );
 };
 
+/**
+ * HttpInterceptor，用於為以 "threads" 開頭的請求新增OpenAI Assistant API專屬標頭。
+ *
+ * @param {HttpRequest<unknown>} req - 要攔截的 HTTP 請求。
+ * @param {HttpHandlerFn} next - 下一個攔截器函數。
+ * @returns {Observable<HttpEvent<unknown>>} - 包含 HTTP 事件的 Observable 對象。
+ */
 export const openAIBetaHeaderHttpInterceptor: HttpInterceptorFn = (
   req: HttpRequest<unknown>,
   next: HttpHandlerFn
@@ -50,6 +71,13 @@ export const openAIBetaHeaderHttpInterceptor: HttpInterceptorFn = (
   return next(req);
 };
 
+/**
+ * HTTP攔截器，用於將所有請求的 URL 修改為 OpenAI API 的Base URL。
+ *
+ * @param {HttpRequest<unknown>} req - 要攔截的 HTTP 請求。
+ * @param {HttpHandlerFn} next - 下一個攔截器函數。
+ * @returns {Observable<HttpEvent<unknown>>} - 包含 HTTP 事件的 Observable 對象。
+ */
 export const openAIBaseURLHttpInterceptor: HttpInterceptorFn = (
   req: HttpRequest<unknown>,
   next: HttpHandlerFn
